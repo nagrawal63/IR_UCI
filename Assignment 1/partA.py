c4414f22a15d4fffa0362330da5c613f890b87e4
Nishchay <nagrawal@ibr.cs.tu-bs.de>
2022-01-06 21:52:17 -0800

    Added trie functionality(although not more efficient) and try catch blocks

--- Assignment 1/partA.py
+++ Assignment 1/partA.py
@@ -2,18 +2,23 @@
 import sys
 
 
-def tokenize(filepath):
+def tokenize(filepath: str):
     tokens = list()
     if filepath is None:
         return tokens
-    with open(filepath, 'r') as f:
-        for line in f:
-            for word in line.split():
-                if word.isalnum():
-                    tokens.append(word.upper())
+
+    try:
+        with open(filepath, 'r') as f:
+            for line in f:
+                for word in line.split():
+                    if word.isalnum():
+                        tokens.append(word.upper())
+    except Exception as e:
+        print(e)
+        return tokens
     return tokens
 
-def computeWordFrequencies(tokensList):
+def computeWordFrequencies(tokensList: list):
     if len(tokensList) == 0:
         return {}
     tokenMap = dict()
@@ -26,7 +31,7 @@ def computeWordFrequencies(tokensList):
     return tokenMap
 
 
-def printFrequencyMap(frequenciesMap):
+def printFrequencyMap(frequenciesMap: dict):
     for key, value in frequenciesMap.items():
         print("<token>{}<freq>{}".format(key, value))
 
@@ -34,7 +39,7 @@ def printFrequencyMap(frequenciesMap):
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         sys.exit(0)
-    # print_hi('PyCharm')
+
     tokens = tokenize(sys.argv[1])
     # print(len(tokens))
     # print(tokens)
--- Assignment 1/partB.py
+++ Assignment 1/partB.py
@@ -3,7 +3,38 @@ import sys
 import partA as tokenizer
 
 
-def getCommonToken(file1, file2):
+class Trie:
+
+    def __init__(self, words: list):
+        # Instantiate the root of trie as an empty dictionary
+        self.root = dict()
+
+        # Creating Trie from the list of given words
+        for word in words:
+            current_dict = self.root
+            for letter in word:
+                current_dict = current_dict.setdefault(letter, {})
+            current_dict['__last_word__'] = '__last_word__'
+
+    def addWord(self, word):
+        current_dict = self.root
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict['__last_word__'] = '__last_word__'
+
+    def findInTrie(self, word, removeEle=False):
+        current_dict = self.root
+        for letter in word:
+            if letter not in current_dict:
+                return False
+            current_dict = current_dict[letter]
+        found = '__last_word__' in current_dict
+        if found and removeEle:
+            del current_dict['__last_word__']
+        return found
+
+
+def getCommonToken(file1: str, file2: str):
     num_of_common_tokens = 0
 
     if file1 == '' or file2 == '':
@@ -11,12 +42,28 @@ def getCommonToken(file1, file2):
 
     tokensMap = tokenizer.computeWordFrequencies(tokenizer.tokenize(file1))
 
-    with open(file2, 'r') as f:
-        for line in f:
-            for word in line.split():
-                if word.isalnum() and word.upper() in tokensMap:
-                    num_of_common_tokens += 1
-                    del tokensMap[word.upper()]
+    try:
+        with open(file2, 'r') as f:
+            for line in f:
+                for word in line.split():
+                    if word.isalnum() and word.upper() in tokensMap:
+                        num_of_common_tokens += 1
+                        # Remove entry from the map so that there is no need to keep track of
+                        # multiple occurrences
+                        del tokensMap[word.upper()]
+    except Exception as e:
+        print(e)
+        sys.exit(0)
+
+    # tokens = tokenizer.tokenize(file1)
+    # trieOfTokens = Trie(tokens)
+    #
+    # with open(file2, 'r') as f:
+    #     for line in f:
+    #         for word in line.split():
+    #             if word.isalnum() and trieOfTokens.findInTrie(word.upper(), True):
+    #                 num_of_common_tokens += 1
+
     return num_of_common_tokens
 
 
@@ -26,3 +73,7 @@ if __name__ == '__main__':
     textfile1 = sys.argv[1]
     textfile2 = sys.argv[2]
     print(getCommonToken(textfile1, textfile2))
+    # tokens = tokenizer.tokenize(textfile1)
+    # root = Trie()
+    # root.make_trie(tokens)
+    # print(root.findInTrie("SME"))
